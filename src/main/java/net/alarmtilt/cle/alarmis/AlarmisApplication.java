7bfd3766b1ccee23a69fb0c06064f3b427e0708c
YABO-AlarmTILT <yabo.github@m-plify.com>
2018-01-12 20:24:38 +0100

    first commit

---
+++ src/main/java/net/alarmtilt/cle/alarmis/AlarmisApplication.java
@@ -0,0 +1,12 @@
+package net.alarmtilt.cle.alarmis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmisApplication.class, args);
+	}
+}
